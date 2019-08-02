77b0db0d7c13ca851a1c35ff91222e359fc49641
Cesar Grachik <50838423+cesargrachik@users.noreply.github.com>
2019-08-02 14:41:42 -0400

    Api Kafka Producer
    Api Kafka Producer

---
+++ src/main/java/com/javatechie/spring/kafka/api/User.java
@@ -0,0 +1,72 @@
+package com.javatechie.spring.kafka.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	
+	private int id;
+	private String name;
+	private String[] address;
+	
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String[] getAddress() {
+		return address;
+	}
+
+	public void setAddress(String[] address) {
+		this.address = address;
+	}
+
+	public User(int id, String name, String[] address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+
+}
